dddcd357e867e0ce0b0a319aa8de5d893995c42b
Haig Armen <haig.armen@gmail.com>
2018-02-15 23:16:05 +0000

    initial commit

--- adafruit_mcp3008.py
+++ adafruit_mcp3008.py
@@ -47,10 +47,17 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
 
 # change these as desired - they're the pins connected from the
 # SPI port on the ADC to the Cobbler
+
 SPICLK = 18
 SPIMISO = 23
 SPIMOSI = 24
 SPICS = 25
+
+# the following pins are for Lots of Pots hat
+# SPICLK = 23
+# SPIMISO = 19
+# SPIMOSI = 21
+# SPICS = 24
 
 # set up the SPI interface pins
 GPIO.setup(SPIMOSI, GPIO.OUT)
@@ -75,28 +82,28 @@ while True:
         pot_adjust = abs(trim_pot - last_read)
 
         if DEBUG:
-                print "trim_pot:", trim_pot
-                print "pot_adjust:", pot_adjust
-                print "last_read", last_read
+                print ("trim_pot:", trim_pot)
+                print ("pot_adjust:", pot_adjust)
+                print ("last_read", last_read)
 
         if ( pot_adjust > tolerance ):
                trim_pot_changed = True
 
         if DEBUG:
-                print "trim_pot_changed", trim_pot_changed
+                print ("trim_pot_changed", trim_pot_changed)
 
         if ( trim_pot_changed ):
                 set_volume = trim_pot / 10.24           # convert 10bit adc0 (0-1024) trim pot read into 0-100 volume level
                 set_volume = round(set_volume)          # round out decimal value
                 set_volume = int(set_volume)            # cast volume as integer
 
-                print 'Volume = {volume}%' .format(volume = set_volume)
+                print ('Volume = {volume}%' .format(volume = set_volume))
                 set_vol_cmd = 'sudo amixer cset numid=1 -- {volume}% > /dev/null' .format(volume = set_volume)
                 os.system(set_vol_cmd)  # set volume
 
                 if DEBUG:
-                        print "set_volume", set_volume
-                        print "tri_pot_changed", set_volume
+                        print ("set_volume", set_volume)
+                        print ("tri_pot_changed", set_volume)
 
                 # save the potentiometer reading for the next loop
                 last_read = trim_pot
