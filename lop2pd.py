2094f712f4df29096b3fb77e2ceb7caffc656aae
Haig Armen <haig@liftstudios.ca>
2018-04-13 17:14:08 -0700

    updated lop2pd

--- lop2pd.py
+++ lop2pd.py
@@ -17,6 +17,8 @@ GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+
+    
 def send2Pd(message=''):
     # Send a message to Pd
     os.system("echo '" + message + "' | pdsend 3000")
@@ -42,36 +44,30 @@ while True:
     input_left = GPIO.input(17)
     input_down = GPIO.input(18)
     input_up = GPIO.input(27)
-    
-    
-    if input_right == False:
-##        print('Right Pressed')
-        message = '8 r;' # make a string for use with Pdsend
-        send2Pd(message)
-        time.sleep(waitTime)
-    if input_left == False:
-##        print('Left Pressed')
-        message = '8 l;' # make a string for use with Pdsend
-        send2Pd(message)
-        time.sleep(waitTime)
-    if input_down == False:
-##        print('Down Pressed')
-        message = '8 d;' # make a string for use with Pdsend
-        send2Pd(message)
-        time.sleep(waitTime)
-    if input_up == False:
-##        print('Up Pressed')
-        message = '8 u;' # make a string for use with Pdsend
-        send2Pd(message)
-        time.sleep(waitTime)
 
     values = [0]*8
     
     for i in range(8):
         values[i] = readadc(i)
         message = str(i) + ' ' + str(values[i]) + ';' # make a string for use with Pdsend
-##        print(message)
+        if input_right == False:
+##        print('Right Pressed')
+            message = '8 r;' # make a string for use with Pdsend
+            time.sleep(waitTime)
+        elif input_left == False:
+##        print('Left Pressed')
+            message = '8 l;' # make a string for use with Pdsend
+            time.sleep(waitTime)
+        elif input_down == False:
+##        print('Down Pressed')
+            message = '8 d;' # make a string for use with Pdsend
+            time.sleep(waitTime)
+        elif input_up == False:
+##        print('Up Pressed')
+            message = '8 u;' # make a string for use with Pdsend
+            time.sleep(waitTime)
+
         send2Pd(message)
 # consider creating a message that has all values in one string rather than separate messages
-    # print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
+    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
     time.sleep(waitTime)
