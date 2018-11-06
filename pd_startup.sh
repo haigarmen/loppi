6faaa4e30d36a0b5a0d706c577c93528816350e0
Haig Armen <haig.armen@gmail.com>
2018-11-06 01:35:00 +0000

    updated startup scripts with comments for installing

--- pd_startup.sh
+++ pd_startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # this script will start Pd in no gui mode
+# this script should go into /etc/profile.d/
+# the ownership should be root:root (chown root:root)
+# the permissions should be (chmod 755)
 
 sudo -H -u pi bash -c bash "echo 'starting Pd now'"
 pd -nogui /home/pi/loppi/step-vibrato.pd &
